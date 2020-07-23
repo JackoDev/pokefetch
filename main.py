5918f4aef4bae33bbffe49c9fed7e5e2082df8be
JackoDev <untaljacko@gmail.com>
2020-07-23 18:55:04 -0500

    [fix] repair broken chain #6

--- main.py
+++ main.py
@@ -21,51 +21,54 @@ def getRequestData():
     response = requests.get(URL + str(input_id))
     # se adiciona el id ingresado, al endpoint
     if response.status_code == 200:
-        response_json = json.loads(response.text)
-        chain = response_json['chain']
+        try:
+            response_json = json.loads(response.text)
+            chain = response_json['chain']
 
-        print('*** Evolution chain # {} ***'.format(input_id))
-        name0 = chain['species']['name']
-        print('Pokemon Name: {}'.format(name0))
+            print('*** Evolution chain # {} ***'.format(input_id))
+            name0 = chain['species']['name']
+            print('Pokemon Name: {}'.format(name0))
 
-        getWeightAndHeight(name0)
-        # llamada a la funcion para obtener ID, Peso y tamaño
-        print('+ Base Stats:')
-        getBaseStats(name0)
-        # Llamada a la funcion para obtener los 6 base-stats
-
-        if chain['evolves_to']:
-            name1 = chain['evolves_to'][0]['species']['name']
-            print('- Evolves to: ')
-            print('Pokemon Name: {}'.format(name1))
-            getWeightAndHeight(name1)
+            getWeightAndHeight(name0)
+            # llamada a la funcion para obtener ID, Peso y tamaño
             print('+ Base Stats:')
-            getBaseStats(name1)
+            getBaseStats(name0)
+            # Llamada a la funcion para obtener los 6 base-stats
 
-            if chain['evolves_to'][0]['evolves_to']:
-                name2 = chain['evolves_to'][0]['evolves_to'][0]
-                ['species']['name']
+            if chain['evolves_to']:
+                name1 = chain['evolves_to'][0]['species']['name']
                 print('- Evolves to: ')
-                print('Pokemon Name: {}'.format(name2))
-                getWeightAndHeight(name2)
+                print('Pokemon Name: {}'.format(name1))
+                getWeightAndHeight(name1)
                 print('+ Base Stats:')
-                getBaseStats(name2)
+                getBaseStats(name1)
 
-                if chain['evolves_to'][0]['evolves_to'][0]['evolves_to']:
-                    name3 = chain['evolves_to'][0]['evolves_to'][0]
-                    ['evolves_to'][0]['species']['name']
+                if chain['evolves_to'][0]['evolves_to']:
+                    name2 = chain['evolves_to'][0]['evolves_to'][0]['species']['name']
                     print('- Evolves to: ')
-                    print('Pokemon Name: {}'.format(name3))
-                    getWeightAndHeight(name3)
+                    print('Pokemon Name: {}'.format(name2))
+                    getWeightAndHeight(name2)
                     print('+ Base Stats:')
-                    getBaseStats(name3)
+                    getBaseStats(name2)
+
+                    if chain['evolves_to'][0]['evolves_to'][0]['evolves_to']:
+                        name3 = chain['evolves_to'][0]['evolves_to'][0]
+                        ['evolves_to'][0]['species']['name']
+                        print('- Evolves to: ')
+                        print('Pokemon Name: {}'.format(name3))
+                        getWeightAndHeight(name3)
+                        print('+ Base Stats:')
+                        getBaseStats(name3)
+                    else:
+                        print
+                        ('*** End of the evo-chain # {} ***'.format(input_id))
                 else:
-                    print
-                    ('*** End of the evo-chain # {} ***'.format(input_id))
+                    print('*** End of the evo-chain # {} ***'.format(input_id))
             else:
                 print('*** End of the evo-chain # {} ***'.format(input_id))
-        else:
-            print('*** End of the evo-chain # {} ***'.format(input_id))
+
+        except:
+            print("Error fetching the data")
     else:
         print('*** ¡Invalid Id! Please Restart the Script and Try again ***')
         # optimizar para validacion de enteros en el inicio del input
