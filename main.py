c9ec8ac03169574c48c3b8015a34c3d1ab56c0e3
JackoDev <untaljacko@gmail.com>
2020-07-23 18:49:15 -0500

    [fix] update indentation, documentation and coments, pep8

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import requests
 import json
 from weightAndHeight import getWeightAndHeight
 from baseStats import getBaseStats
-from genDict import generateDicts
+
+""" This is the main file for execute the search for the
+    pokemon's evolution-chain Id, input by the user in
+    the message prompted after the script execution """
 
 
 def getRequestData():
+    """ Main function to execute the GET petition to the API """
     URL = "https://pokeapi.co/api/v2/evolution-chain/"
-    # evoChain = []  # pendiente optimizar para obtener un array y almacenar cada evolucion para evitar los if anidados recorriendo el json con un loop
+    # evoChain = []  Se puede optimizar el algoritmo manejando un array y
+    # almacenando cada evolucion para evitar los if anidados recorriendo el
+    # json con un loop
 
-    print('Please enter an Id')   # se toman los datos del usuario
+    print('Please enter an Id')
     input_id = input()
 
-    response = requests.get(URL + str(input_id))  # se adiciona el id al endpoint
+    response = requests.get(URL + str(input_id))
+    # se adiciona el id ingresado, al endpoint
     if response.status_code == 200:
         response_json = json.loads(response.text)
         chain = response_json['chain']
@@ -20,12 +27,14 @@ def getRequestData():
         print('*** Evolution chain # {} ***'.format(input_id))
         name0 = chain['species']['name']
         print('Pokemon Name: {}'.format(name0))
-        
+
         getWeightAndHeight(name0)
+        # llamada a la funcion para obtener ID, Peso y tamaño
         print('+ Base Stats:')
         getBaseStats(name0)
+        # Llamada a la funcion para obtener los 6 base-stats
 
-        if chain['evolves_to']: 
+        if chain['evolves_to']:
             name1 = chain['evolves_to'][0]['species']['name']
             print('- Evolves to: ')
             print('Pokemon Name: {}'.format(name1))
@@ -33,37 +42,34 @@ def getRequestData():
             print('+ Base Stats:')
             getBaseStats(name1)
 
-            if chain['evolves_to'][0]['evolves_to']: 
-                name2 = chain['evolves_to'][0]['evolves_to'][0]['species']['name']
+            if chain['evolves_to'][0]['evolves_to']:
+                name2 = chain['evolves_to'][0]['evolves_to'][0]
+                ['species']['name']
                 print('- Evolves to: ')
                 print('Pokemon Name: {}'.format(name2))
                 getWeightAndHeight(name2)
                 print('+ Base Stats:')
                 getBaseStats(name2)
-        
-                if chain['evolves_to'][0]['evolves_to'][0]['evolves_to']: 
-                    name3 = chain['evolves_to'][0]['evolves_to'][0]['evolves_to'][0]['species']['name']
+
+                if chain['evolves_to'][0]['evolves_to'][0]['evolves_to']:
+                    name3 = chain['evolves_to'][0]['evolves_to'][0]
+                    ['evolves_to'][0]['species']['name']
                     print('- Evolves to: ')
                     print('Pokemon Name: {}'.format(name3))
                     getWeightAndHeight(name3)
                     print('+ Base Stats:')
                     getBaseStats(name3)
                 else:
-                    print('*** End of the evolution chain # {} ***'.format(input_id))
-            else:    
-                print('*** End of the evolution chain # {} ***'.format(input_id)) 
+                    print
+                    ('*** End of the evo-chain # {} ***'.format(input_id))
+            else:
+                print('*** End of the evo-chain # {} ***'.format(input_id))
         else:
-            print('*** End of the evolution chain # {} ***'.format(input_id))
+            print('*** End of the evo-chain # {} ***'.format(input_id))
     else:
-        print('*** ¡Invalid Id! Please Restart the Script and Try again ***') #optimizar para validacion de enteros en el inicio del input
+        print('*** ¡Invalid Id! Please Restart the Script and Try again ***')
+        # optimizar para validacion de enteros en el inicio del input
+
 
 if __name__ == '__main__':
     getRequestData()
-    
-
-
-    
- 
-
-
-    
