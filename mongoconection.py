2e149907da1f81b7fc3e8513551bf2dca5ca360f
JackoDev <untaljacko@gmail.com>
2020-07-23 19:34:47 -0500

    [fix] added comments, tested and remove line with unnecesary print

--- mongoconection.py
+++ mongoconection.py
@@ -1,13 +1,19 @@
 import pymongo
+""" This module contains the info for connect and login
+    with the mongoDB Atlas cloud, for send the dicts
+    with the info fetched for the later use in  the
+    web service """
 
+
+# Atlas mongoDB data login
 client = pymongo.MongoClient("mongodb+srv://pokeusuario:pokepassword@pokecluster.z7r13.gcp.mongodb.net/poketest?retryWrites=true&w=majority")
 my_db = client.poketest
 my_collection = my_db.pokemones
 
 try:
+    # For test the mongoDB connection
     print("MongoDB version is %s" %
             client.server_info()['version'])
-    print('MongoDB connection established')
 except pymongo.errors.OperationFailure as error:
     print(error)
     quit(1)
